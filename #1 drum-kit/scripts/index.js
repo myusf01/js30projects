a9c658593913c00eb3ff79cdac67dd2a2e264f7c
Muhammed Yusuf <myusuf2121@gmail.com>
2021-06-28 17:51:59 +0300

    Play sounds by click event.

--- #1 drum-kit/scripts/index.js
+++ #1 drum-kit/scripts/index.js
@@ -1,10 +1,23 @@
+function playOnClick(e) {
+  const getAudio = e.path[0].outerText.toLowerCase()
+  const audio = document.querySelector(`audio[data-key="${getAudio}"]`)
+  const key = document.querySelector(`.key[data-key="${getAudio}"]`)
+  if (!audio) return //stop function if undefined key pressed.
+  // Set current audio time to 0, to play sound continuously.
+  audio.currentTime = 0
+  // Play sound
+  audio.play()
+  // Add playing class to class list
+  // By this we are adding transition to our div
+  key.classList.add('playing')
+
+  
+}
 function removeTransition(e) {
-    console.log(e);
   if (e.animationName != 'pulse') return
   // Remove playing class from class list
   // By this we backwarding our transition
   // And the div is turnin to its normal state.
-  console.log(this);
   this.classList.remove('playing')
 }
 function playSound(e) {
@@ -15,7 +28,6 @@ function playSound(e) {
   const key = this.document.querySelector(`.key[data-key="${e.key}"]`)
 
   if (!audio) return //stop function if undefined key pressed.
-
   // Set current audio time to 0, to play sound continuously.
   audio.currentTime = 0
   // Play sound
@@ -27,11 +39,10 @@ function playSound(e) {
 
 // Select tags that have key class
 const keys = document.querySelectorAll('.key')
-console.log(keys);
 // Selecct each tag addEventListener that has transitioned by 'transitionend'
 // Then remove the tag to revert the transition.
 keys.forEach(key => key.addEventListener('animationend', removeTransition))
-
+keys.forEach(key => key.addEventListener('mousedown',playOnClick))
 // Add event listener to listen when key pressed then
 //  start playSound function to play sound.
 window.addEventListener('keydown', playSound)
